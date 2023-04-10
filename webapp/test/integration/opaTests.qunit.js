cb3ffe68d754cdd39a2cdbca3158c456f51035f2
HugoStehling <130396480+HugoStehling@users.noreply.github.com>
2023-04-10 15:15:44 -0300

    "Semana dominando SAP Fiori do Zero"
    "Semana dominando SAP Fiori do Zero"

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zbuscarprodutos/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zbuscar_produtos/test/unit/controller/main.controller"
+], function () {
+	"use strict";
+});
